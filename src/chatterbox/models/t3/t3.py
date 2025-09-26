03c485812b5ea193a10477da8628a68e7edb2624
Raphael Fritz <raphael.fritz@articlett.de>
2025-09-26 14:53:26 +0200

    fix: error when trying to use it for CPU

--- src/chatterbox/models/t3/t3.py
+++ src/chatterbox/models/t3/t3.py
@@ -348,8 +348,8 @@ class T3(nn.Module):
         # optimizations
         max_cache_len=1500,
         initial_forward_pass_backend="eager",
-        generate_token_backend="cudagraphs-manual",
-        # generate_token_backend="eager",
+        #generate_token_backend="cudagraphs-manual",
+        generate_token_backend="eager",
         stride_length=4,
         skip_when_1=True,
         benchmark_t3=False,
