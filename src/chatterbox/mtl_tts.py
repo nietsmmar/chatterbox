1737309a080251a0fc2fc5e43855c92a705e32d8
Raphael Fritz <raphael.fritz@articlett.de>
2025-09-26 14:55:37 +0200

    fix watermark error

--- src/chatterbox/mtl_tts.py
+++ src/chatterbox/mtl_tts.py
@@ -150,7 +150,6 @@ class ChatterboxMultilingualTTS:
         self.tokenizer = tokenizer
         self.device = device
         self.conds = conds
-        self.watermarker = perth.PerthImplicitWatermarker()
 
     @classmethod
     def get_supported_languages(cls):
@@ -304,5 +303,4 @@ class ChatterboxMultilingualTTS:
                 ref_dict=self.conds.gen,
             )
             wav = wav.squeeze(0).detach().cpu().numpy()
-            watermarked_wav = self.watermarker.apply_watermark(wav, sample_rate=self.sr)
-        return torch.from_numpy(watermarked_wav).unsqueeze(0)
+        return torch.from_numpy(wav).unsqueeze(0)
